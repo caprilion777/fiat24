21532703c9efc83e29af618909cd3727f6f3da58
caprilion777 <capri.lion.777@gmail.com>
2025-09-06 16:18:20 +0300

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="w-full bg-black text-white py-6 flex flex-col items-center justify-center text-center text-[12px] md:text-[14px] shadow-lg footer-shadow">
     <div>© 2021–2025 Fiat24.ru — сервис по организации обмена криптовалюты.</div>
-    <div>Адрес: Москва, Москва-Сити, Башня &quot;Федерация&quot;</div>
+    <div>Наши адреса:<br />Россия, Москва, Москва-Сити, Башня «Федерация»<br />ОАЭ, Дубай, Downtown Dubai, Burj Khalifa</div>
     <div>E-mail: <a href="mailto:info@fiat24.ru" className="underline">info@fiat24.ru</a></div>
   </footer>
 );
